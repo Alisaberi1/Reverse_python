c94266e5080162c30553e0d29f361c6d00b5ebc5
Ali $@ <90723218+Alisaberi1@users.noreply.github.com>
2022-05-20 15:07:53 +0430

    Add files via upload

---
+++ Reverse_python.py
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+  <head>
+    <title>Quera</title>
+    <meta name="description"
+      content="از آموزش و تمرین برنامه‌نویسی تا رقابت و استخدام در بهترین شرکت‌ها! در Quera، بزرگترین جامعه‌ی توسعه‌دهندگان ایرانی">
+
+    <meta content="text/html" charset="UTF-8">
+    <meta name="fontiran.com:license" content="56AXFY">
+    
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <meta property="og:site_name" content="Quera" />
+    
+      <meta property="og:title" content="Quera" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/static/images/logo/logo-q.2-191a914662ba.png" />
+      
+    
+    
+      
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/images/logo/favicon/apple-touch-icon.2-2887f2aad086.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/images/logo/favicon/favicon-32x32.2-b5be5ba884c5.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/images/logo/favicon/favicon-16x16.2-4bcbe4f363d7.png">
+      <link rel="manifest" href="/static/images/logo/favicon/site.2-eee0bbeb0fd7.webmanifest">
+      <link rel="mask-icon" href="/static/images/logo/favicon/safari-pinned-tab.2-c497e3a2ff0f.svg" color="#5bbad5">
+      <link rel="shortcut icon" href="/static/images/logo/favicon/favicon.2-53ba381ba55d.ico">
+      <meta name="apple-mobile-web-app-title" content="Quera">
+      <meta name="application-name" content="Quera">
+      <meta name="msapplication-TileColor" content="#da532c">
+      <meta name="msapplication-config" content="/static/images/logo/favicon/browserconfig.2-0049c1f5e338.xml">
+      <meta name="theme-color" content="#09c">
+    
+    
+
+
+  <script>
+    /* beautify ignore:start */
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TDRQP6V');
+    /* beautify ignore:end */
+  </script>
+
+
+    <script type="text/javascript" src="/static/js/reverse.2-ec1aeeae5ca6.js"></script>
+    <link rel="stylesheet" href="/static/vendor/jquery-ui/themes/flick/jquery-ui.min.2-74145a385e47.css" />
+    <script type="text/javascript" src="/static/vendor_common/common.min.2-ff5d1031dabd.js"></script>
+    <link rel="stylesheet" type="text/css" href="/static/vendor_common/common.min.2-c21277d78369.css" />
+    <link rel="stylesheet" type="text/css" href="/static/vendor/semantic/semantic.rtl.min.2-87f68286e0ba.css" />
+    <link rel="stylesheet" type="text/css" href="/static/css/style.2-7e43975119dd.css" />
+    <link rel="stylesheet" type="text/css" href="/static/semanticui/style.2-b3ef4156e542.css" />
+    <link rel="preload" href="/static/fonts/iranyekan/fonts/woff2/IRANYekanWebRegular.2-9b47f3164ee9.woff2" as="font"
+      type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" href="/static/fonts/iranyekan/fonts/woff2/IRANYekanWebBold.2-980f51e04b7c.woff2" as="font"
+      type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" href="/static/fonts/iranyekan/fonts/woff2/IRANYekanWebLight.2-1a7cfbfeabaf.woff2" as="font"
+      type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" href="/static/fonts/iranyekan/fonts/woff2/IRANYekanWebMedium.2-3c3b4ec2f970.woff2" as="font"
+      type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" href="/static/vendor/semantic/themes/default/assets/fonts/icons.2-0ab54153eeec.woff2" as="font"
+      type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" href="/static/vendor/semantic/themes/default/assets/fonts/brand-icons.2-e8c322de9658.woff2" as="font"
+      type="font/woff2" crossorigin="anonymous">
+    <script type="text/javascript" src="/static/js/main.2-17d84136ed9c.js"></script>
+    <script type="text/javascript" src="/static/js/notebook.2-0c223d43deb5.js"></script>
+    <script type="text/javascript" src="/static/js/markdown-moratab.2-e2dda6943e4b.js"></script>
+    <script src="/static/vendor/clipboard/clipboard.min.2-f06c52bfddb4.js" type="text/javascript"></script>
+    <script src="/static/vendor/prism/prism.min.2-683a0944f523.js" data-manual></script>
+    <link rel="stylesheet" type="text/css" href="/static/vendor/prism/prism.2-e59b2728e2fc.css">
+    <script src="/static/notifications/notify.2-adeb6bd4d98d.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="/static/vendor/katex/katex.min.2-852d91d247c4.css" />
+    <link rel="stylesheet" href="/static/fonts/peyda/css/fontiran.2-fd9d51925025.css">
+
+    
+    
+
+    
+  
+  <script type="text/javascript" src="/static/js/quera_submissions.2-6ca5fa9940a5.js"></script>
+  <script src="/static/vendor/ace-builds/ace.2-83603cd2a063.js" type="text/javascript" charset="utf-8"></script>
+  <script src="/static/vendor/ace-builds/ext-modelist.2-10a6e9aa10e1.js"></script>
+  <script src="/static/vendor/ace-builds/ext-language_tools.2-5e18cb529b96.js"></script>
+  <link rel="stylesheet" href="/static/semanticui/date-time-picker/css/semantic-date-picker.2-34ad5cadbdf4.css" />
+  <script>
+    let path = "/static/vendor/ace-builds/ace.2-83603cd2a063.js";
+    path = path.substring(0, path.lastIndexOf("/") + 1);
+    ace.config.set('basePath', path);
+  </script>
+  <link rel="stylesheet" type="text/css" href="/static/college/css/introjs.2-6f2febfc044c.css" />
+  <link rel="stylesheet" type="text/css" href="/static/college/css/introjs-rtl.2-d4336c2f6298.css" />
+  <style>
+    .introjs-button {
+      font-family: IRANYekan;
+    }
+  </style>
+
+    
+      
+    
+  </head>
+
+  <body dir="rtl" style="">
+
+    
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDRQP6V" height="0" width="0"
+    style="display:none;visibility:hidden"></iframe></noscript>
+
+
+
+    
+    <div class="pusher" id="qu-scroll">
+      
+      
+      <div dir="ltr" id="qu-hj"></div>
+
+      <script type="text/javascript" charset="utf-8">
+        quera = {};
+        quera.finish_time = moment('0');
+        quera.extra_time = moment.duration(0, 'seconds');
+        quera.server_first_time = '2022-05-20T15:01:57.298583+04:30';
+        quera.offset = moment('2022-05-20T15:01:57.298997+04:30').diff(moment());
+        quera.time = moment();
+        quera.times = [];
+        
+
+      </script>
+      
+        <script>
+          
+
+        </script>
+      
+      <div id="body_container">
+        
+  <div id="college"></div>
+  
+
+<script type="module" src="/static/react/assets/college.e31b266a.js"></script>
+<script nomodule="" src="/static/react/assets/polyfills-legacy.6a858d0c.js"></script>
+<script nomodule="" src="/static/react/assets/college.e31b266a.js"></script>
+  <script src="/static/semanticui/date-time-picker/js/persian-date.2-7d43add75b87.js"></script>
+  <script src="/static/semanticui/date-time-picker/js/semantic-date-picker.2-737d1b8a4db3.js"></script>
+
+      </div>
+      
+    </div>
+
+    <script type="text/javascript" src="/static/js/quera_timer.2-cfcac0b85e9a.js"></script>
+    
+  </body>
+
+</html>
